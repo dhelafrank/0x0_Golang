ab53707cf3e557a0601bd360299b766bd658663b
dhelafrank <ubongabasijerome8@gmail.com>
2024-06-23 13:19:06 +0100

    Init: Go Binaries

---
+++ hello.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+}
+
+// We can create binary files in go by simply running '~go build hello.go'
+// or achitecture specific binary by running '~GOOS=windows OGARCH=amd64 go build hello.go'
